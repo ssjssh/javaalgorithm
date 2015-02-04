13a35cede5da71270ea17628a9d5c87e9a38f9ce
shengshijun <ssj970763342@163.com>
2015-02-04 14:09:26 +0800

    BigInt完成骨架。

--- src/main/java/ssj/algorithm/math/BigInt.java
+++ src/main/java/ssj/algorithm/math/BigInt.java
@@ -4,11 +4,13 @@ import com.google.common.base.Preconditions;
 import ssj.algorithm.collections.LinkedList;
 import ssj.algorithm.string.StringBuilder;
 
+import java.util.Comparator;
+
 /**
  * 使用链表实现BigInt
  * Created by shenshijun on 15/2/3.
  */
-public class BigInt {
+public class BigInt implements Comparator<BigInt> {
     private LinkedList<Character> _values;
     private boolean _sign;
 
@@ -18,21 +20,55 @@ public class BigInt {
         addTailBit(Math.abs(origin));
     }
 
+    public BigInt() {
+        _values = new LinkedList<>();
+        _sign = true;
+        addTailBit(0);
+    }
+
+    public void setPositive() {
+        _sign = true;
+    }
+
+    public void setNegative() {
+        _sign = false;
+    }
+
+    public int bitCount() {
+        return _values.size();
+    }
+
+    public void setBit(int index, int bit) {
+        Preconditions.checkPositionIndex(index, bitCount());
+        Preconditions.checkArgument(bit >= 0 && bit < 10);
+        _values.set(index, intToChars(bit)[0]);
+    }
+
+    private char[] intToChars(int number) {
+        String number_string = String.valueOf(number);
+        return number_string.toCharArray();
+    }
+
+
     public void addTailBit(int n) {
         Preconditions.checkArgument(n >= 0);
-        String number_string = String.valueOf(n);
-        for (int i = 0; i < number_string.length(); i++) {
-            _values.append(number_string.charAt(i));
+        for (char c : intToChars(n)) {
+            _values.add(c);
         }
 
     }
 
     public void addHeadBit(int n) {
         Preconditions.checkArgument(n >= 0);
-        String number_string = String.valueOf(n);
-        for (int i = 0; i < number_string.length(); i++) {
-            _values.appendHead(number_string.charAt(i));
+        for (char c : intToChars(n)) {
+            _values.appendHead(c);
         }
+    }
+
+    @Override
+    public int compare(BigInt o1, BigInt o2) {
+//        return o1.sub(o2);
+        return 0;
     }
 
     @Override
@@ -55,7 +91,22 @@ public class BigInt {
         return result;
     }
 
+    public boolean isPositive() {
+        return _sign;
+    }
+
+    public boolean isNegative() {
+        return !isPositive();
+    }
+
     public BigInt add(BigInt other) {
+        BigInt result = new BigInt();
+        if (other.isNegative() && isPositive()) {
+            other.setPositive();
+            return sub(other);
+        } else if (other.isPositive() && isNegative()) {
+//            other.add()
+        }
         return null;
     }
 
@@ -68,6 +119,14 @@ public class BigInt {
     }
 
     public BigInt div(BigInt other) {
+        return null;
+    }
+
+    private LinkedList<Character> _add(LinkedList<Character> other) {
+        return null;
+    }
+
+    private LinkedList<Character> _sub(LinkedList<Character> other) {
         return null;
     }
 
