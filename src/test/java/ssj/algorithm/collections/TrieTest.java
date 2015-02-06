0db92ef68446fcdb9020ac43f72aa1debf4d131d
shengshijun <ssj970763342@163.com>
2015-02-06 17:50:31 +0800

    完成Trie

--- src/test/java/ssj/algorithm/collections/TrieTest.java
+++ src/test/java/ssj/algorithm/collections/TrieTest.java
@@ -12,11 +12,11 @@ public class TrieTest {
         Trie trie = new Trie();
         trie.add("1234567");
         trie.add("12346789101112");
-//        trie.add("12346799101112");
-//        trie.add("123467109101112");
-//        trie.add("12346711101112");
-//        trie.add("12346713101112");
-//        trie.add("12346715101112");
-//        System.out.println(trie.search("1234567"));
+        trie.add("12346799101112");
+        trie.add("123467109101112");
+        trie.add("12346711101112");
+        trie.add("12346713101112");
+        trie.add("12346715101112");
+        System.out.println(trie.search("1234567"));
     }
 }
